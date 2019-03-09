4c6652313c3f1d709e32dcb6f2e7f532480a1df5
frank <frank@qlimix.net>
2019-03-09 16:22:34 +0100

    only pass exception in kernel exceptions

--- src/Exception/KernelException.php
+++ src/Exception/KernelException.php
@@ -3,7 +3,12 @@
 namespace Qlimix\Kernel\Exception;
 
 use Exception;
+use Throwable;
 
 final class KernelException extends Exception
 {
+    public function __construct(?Throwable $previous = null)
+    {
+        parent::__construct('', 0, $previous);
+    }
 }
